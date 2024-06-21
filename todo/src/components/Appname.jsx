894d969000a539447cec3bd1cf59ba0bc47c3c03
dhruvprajapati2 <dhruv9870a@gmail.com>
2024-06-21 11:00:42 +0530

    first commit

---
+++ todo/src/components/Appname.jsx
@@ -0,0 +1,7 @@
+import styles from './AppName.module.css';
+function Appname()
+{
+return <h1>Project App</h1>;
+
+}
+export default Appname;
---
+++ todo/src/components/WelcomeMessage.jsx
@@ -0,0 +1,7 @@
+import styles from "./WelcomeMessage.module.css";
+
+const WelcomeMessage = () => {
+  return <p className={styles.welcome}>No Tasks Right now</p>;
+};
+
+export default WelcomeMessage;
